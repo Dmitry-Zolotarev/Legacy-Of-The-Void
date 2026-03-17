2129a0f0b64716ef8b7e4bb171df8b5c1aaa455d
unknown <dimazed04@gmail.com>
2026-03-17 13:25:41 +0700

    Добавлена механика тренировок в MVP формате

--- Assets/Game/Scripts/Items/RayGun.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Item", menuName = "Inventory/RayGun")]
-public class RayGun : Item
-{
-    public int damageIncrease = 2, rangeIncrease = 10;
-    public float fireRate = 0.5f;
-}
