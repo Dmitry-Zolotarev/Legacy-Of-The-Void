aa306249c5d40b986dfc1f771f54a1bc40eaaaf2
Дмитрий <dimazed04@gmail.com>
2026-04-07 18:38:10 +0700

    Bug fixes

--- Assets/Game/Scripts/Core/TrainingSystem.cs
+++ Assets/Game/Scripts/Core/TrainingSystem.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(ParticleSpawner))]
 public class TrainingSystem : MonoBehaviour
@@ -8,6 +10,12 @@ public class TrainingSystem : MonoBehaviour
     [SerializeField] private TextMeshProUGUI BodyLabel;
     [SerializeField] private int StartBodyBonus = 1;
     [SerializeField] private int ElixirPower = 2;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Image background;
+    [SerializeField] private Sprite gymBackground;
+    [SerializeField] private Sprite combatBackground;
+    [SerializeField] private GameObject mainHub;
+    [SerializeField] private float trainingTime = 2f;
     private ParticleSpawner spawner;
     private int BodyBonus = 1;
 
@@ -15,9 +23,9 @@ public class TrainingSystem : MonoBehaviour
     {
         spawner = GetComponent<ParticleSpawner>();
     }
-    private void OnEnable()
+    private void FixedUpdate()
     {
-        UpdateLabels();
+        UpdateLabels();        
     }
     private void UpdateLabels()
     {
@@ -25,22 +33,36 @@ public class TrainingSystem : MonoBehaviour
     }
     public void TrainBody()
     {
+        StartCoroutine(TrainBodyCoroutine());   
+    }
+    private IEnumerator TrainBodyCoroutine()
+    {      
         var master = GameCore.Instance.Master;
         BodyBonus = StartBodyBonus;
-        
-        if(master.Body < master.MaxBody)
+
+        if (master.Body < master.MaxBody)
         {
+            mainHub.SetActive(false);
+            background.sprite = gymBackground;
+            animator.gameObject.SetActive(true);
+            animator.Play("Push Up");
+            yield return new WaitForSeconds(trainingTime);
+
+            background.sprite = combatBackground;
+            animator.gameObject.SetActive(false);
+            mainHub.SetActive(true);
+
             if (master.BodyElixirs > 0)
             {
                 BodyBonus *= ElixirPower;
-                master.BodyElixirs--;          
+                master.BodyElixirs--;
                 UpdateLabels();
                 spawner.Spawn(BodyElixirsLabel.transform, $"-1", Color.red);
             }
             int bodyTrained = master.TrainBody(BodyBonus);
             spawner.Spawn(BodyLabel.transform, $"+{bodyTrained}", Color.green);
-            
-            GameCore.Instance.AdvanceTime(1); 
-        }       
+
+            GameCore.Instance.AdvanceTime(1);
+        }
     }
 }
--- Assets/Game/Scripts/UI/TravelToFightButton.cs
+++ Assets/Game/Scripts/UI/TravelToFightButton.cs
@@ -22,31 +22,14 @@ public class TravelToFightButton : MonoBehaviour
     {
         if((int)demon < 4) gameObject.SetActive(!GameCore.Instance.Enemies[(int)demon].IsDead);
     }
-    private IEnumerator NotEnoughRank()
-    {
-        tooltip.ShowTooltip("У игрока слишком низкий ранг!");
-        yield return new WaitForSeconds(1.5f);
-        tooltip.ShowTooltip();
-    }
     public void TravelToFight()
     {
-        if (GameCore.Instance.Master.CurrentRank < (int)enemy.Rank)
-        {
-            StartCoroutine(NotEnoughRank());
-            return;
-        }
-        try
-        {
-            GameCore.Instance.SelectedDemon = demon;
-
-            BattleLaunchData launchData = new BattleLaunchData(enemy);
-
-            TravelSystem.Instance.EnemyImage.sprite = enemySprite;
-            TravelSystem.Instance.SelectedLevel = this;
-            TravelSystem.Instance.SilverBonus = GameCore.Instance.random.Next(minSilverBonus, maxSilverBonus + 1);
-            TravelSystem.Instance.BattleLaunchButton.BattleData = launchData;
-            TravelSystem.Instance.ShowFightDialog(tooltip.tooltipText);
-        }
-        catch { }   
+        GameCore.Instance.SelectedDemon = demon;
+        BattleLaunchData launchData = new BattleLaunchData(enemy);
+        TravelSystem.Instance.EnemyImage.sprite = enemySprite;
+        TravelSystem.Instance.SelectedLevel = this;
+        TravelSystem.Instance.SilverBonus = GameCore.Instance.random.Next(minSilverBonus, maxSilverBonus + 1);
+        TravelSystem.Instance.BattleLaunchButton.BattleData = launchData;
+        TravelSystem.Instance.ShowFightDialog(tooltip.tooltipText);
     }
 }
